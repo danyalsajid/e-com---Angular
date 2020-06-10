4edbba6341bbcab49155776c9c98c94971c7b613
DanyalSajid00 <danyalsajid123@gmail.com>
2020-06-10 19:12:23 +0500

    app structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'product', component: ProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
