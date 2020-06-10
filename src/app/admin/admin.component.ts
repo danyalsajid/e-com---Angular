28300b05b2bfd12c90f7f1dd09b009a312edf170
DanyalSajid00 <danyalsajid123@gmail.com>
2020-06-11 00:25:38 +0500

    admin structure

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -6,14 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-  options = ["one", "two"];
+  sideMenuOptions: { name: string, icon: string, active: boolean }[] = [
+    { name: "Add Home Page", icon: "fa-home", active: true },
+    { name: "View/Edit Home Page", icon: "fa-home", active: false },
+    { name: "Add Category", icon: "fa-list", active: false },
+    { name: "View/Edit Category", icon: "fa-list", active: false },
+    { name: "Add Product", icon: "fa-archive", active: false },
+    { name: "View/Edit Product", icon: "fa-archive", active: false },
+    { name: "View/Edit Order", icon: "fa-file-text-o", active: false },
+    { name: "View/Edit Complaint", icon: "fa-exclamation-circle", active: false },
+  ];
+
+  selectedSideMenuOption: string = this.sideMenuOptions[0].name;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  search($event){
-    console.log($event);
+  onSelectMenu(sideMenuOption: string) {
+    this.selectedSideMenuOption = sideMenuOption;
+    for (const iterator of this.sideMenuOptions) {
+      if (sideMenuOption === iterator.name) {
+        iterator.active = true;
+      } else {
+        iterator.active = false;
+      }
+    }
   }
+
+  logout(){
+  }
+
 
 }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,10 +4,18 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { SharedModule } from '../shared/shared.module';
+import { AddHomeComponent } from './add-home/add-home.component';
+import { ViewHomeComponent } from './view-home/view-home.component';
+import { AddCategoryComponent } from './add-category/add-category.component';
+import { ViewCategoryComponent } from './view-category/view-category.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { ViewOrderComponent } from './view-order/view-order.component';
+import { ViewComplaintComponent } from './view-complaint/view-complaint.component';
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, AddHomeComponent, ViewHomeComponent, AddCategoryComponent, ViewCategoryComponent, AddProductComponent, ViewProductComponent, ViewOrderComponent, ViewComplaintComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
