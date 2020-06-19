7d20e414d64419a7b2da38870adc4a242a40f1c7
DanyalSajid00 <danyalsajid123@gmail.com>
2020-06-19 16:14:45 +0500

    product detail

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -5,10 +5,11 @@ import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
 @NgModule({
-  declarations: [ProductComponent, ProductListComponent],
+  declarations: [ProductComponent, ProductListComponent, ProductDetailComponent],
   imports: [
     CommonModule,
     ProductRoutingModule,
