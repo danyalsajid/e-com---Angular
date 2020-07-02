e1b2996432c28195976317a9ba8307e85d28c7ba
DanyalSajid00 <danyalsajid123@gmail.com>
2020-07-02 13:25:49 +0500

    new arrivals in home page

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/product/product.service';
+import { ProductData } from 'src/app/shared/models/productData';
 
 @Component({
   selector: 'app-home-page',
@@ -7,104 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  products = [];
-  constructor() { }
+  productData: ProductData[] = [];
+  productIds: string[] = [];
+  constructor(private service: ProductService) { }
 
   ngOnInit(): void {
-    this.products = [
-      {
-        name: "Product 1",
-        category: "Category",
-        percentOff: 30,
-        price: 300,
-        imgUrl: "../../assets/electronics-banner-02.jpg",
-        img2Url: "../../assets/electronics-banner-02.jpg",
-        img3Url: "../../assets/electronics-banner-02.jpg",
-        rating: 4.5,
-        description: "Ad illum natoque volutpat leo curabitur est nisi reprehenderit quisque illo ullam scelerisque viverra taciti voluptatum adipiscing omnis vel augue convallis anim dis quis et molestiae, eos aenean corrupti neque? Interdum, quisque diam molestie porta iaculis earum? Non magni bibendum eum fugiat, fringilla donec",
-        reviews: [
-          {
-            reviewer: "Name Surename",
-            date: "5 Sep 2020",
-            review: "Porta corporis nibh. Adipisci maiores dui torquent porttitor wisi necessitatibus lorem perspiciatis repudiandae ad nesciunt deleniti facilisi, est orci libero.",
-          },
-          {
-            reviewer: "Name Surename",
-            date: "5 Sep 2020",
-            review: "Porta corporis nibh. Adipisci maiores dui torquent porttitor wisi necessitatibus lorem perspiciatis repudiandae ad nesciunt deleniti facilisi, est orci libero.",
-          },
-        ]
-      },
-      {
-        name: "Product 1",
-        category: "Category",
-        percentOff: 30,
-        price: 300,
-        imgUrl: "../../assets/electronics-banner-02.jpg",
-        img2Url: "../../assets/electronics-banner-02.jpg",
-        img3Url: "../../assets/electronics-banner-02.jpg",
-        rating: 4.5,
-        description: "Ad illum natoque volutpat leo curabitur est nisi reprehenderit quisque illo ullam scelerisque viverra taciti voluptatum adipiscing omnis vel augue convallis anim dis quis et molestiae, eos aenean corrupti neque? Interdum, quisque diam molestie porta iaculis earum? Non magni bibendum eum fugiat, fringilla donec",
-        reviews: [
-          {
-            reviewer: "Name Surename",
-            date: "5 Sep 2020",
-            review: "Porta corporis nibh. Adipisci maiores dui torquent porttitor wisi necessitatibus lorem perspiciatis repudiandae ad nesciunt deleniti facilisi, est orci libero.",
-          },
-          {
-            reviewer: "Name Surename",
-            date: "5 Sep 2020",
-            review: "Porta corporis nibh. Adipisci maiores dui torquent porttitor wisi necessitatibus lorem perspiciatis repudiandae ad nesciunt deleniti facilisi, est orci libero.",
-          },
-        ]
-      },
-      {
-        name: "Product 1",
-        category: "Category",
-        percentOff: 30,
-        price: 300,
-        imgUrl: "../../assets/electronics-banner-02.jpg",
-        img2Url: "../../assets/electronics-banner-02.jpg",
-        img3Url: "../../assets/electronics-banner-02.jpg",
-        rating: 4.5,
-        description: "Ad illum natoque volutpat leo curabitur est nisi reprehenderit quisque illo ullam scelerisque viverra taciti voluptatum adipiscing omnis vel augue convallis anim dis quis et molestiae, eos aenean corrupti neque? Interdum, quisque diam molestie porta iaculis earum? Non magni bibendum eum fugiat, fringilla donec",
-        reviews: [
-          {
-            reviewer: "Name Surename",
-            date: "5 Sep 2020",
-            review: "Porta corporis nibh. Adipisci maiores dui torquent porttitor wisi necessitatibus lorem perspiciatis repudiandae ad nesciunt deleniti facilisi, est orci libero.",
-          },
-          {
-            reviewer: "Name Surename",
-            date: "5 Sep 2020",
-            review: "Porta corporis nibh. Adipisci maiores dui torquent porttitor wisi necessitatibus lorem perspiciatis repudiandae ad nesciunt deleniti facilisi, est orci libero.",
-          },
-        ]
-      },
-      {
-        name: "Product 1",
-        category: "Category",
-        percentOff: 30,
-        price: 300,
-        imgUrl: "../../assets/electronics-banner-02.jpg",
-        img2Url: "../../assets/electronics-banner-02.jpg",
-        img3Url: "../../assets/electronics-banner-02.jpg",
-        rating: 4.5,
-        description: "Ad illum natoque volutpat leo curabitur est nisi reprehenderit quisque illo ullam scelerisque viverra taciti voluptatum adipiscing omnis vel augue convallis anim dis quis et molestiae, eos aenean corrupti neque? Interdum, quisque diam molestie porta iaculis earum? Non magni bibendum eum fugiat, fringilla donec",
-        reviews: [
-          {
-            reviewer: "Name Surename",
-            date: "5 Sep 2020",
-            review: "Porta corporis nibh. Adipisci maiores dui torquent porttitor wisi necessitatibus lorem perspiciatis repudiandae ad nesciunt deleniti facilisi, est orci libero.",
-          },
-          {
-            reviewer: "Name Surename",
-            date: "5 Sep 2020",
-            review: "Porta corporis nibh. Adipisci maiores dui torquent porttitor wisi necessitatibus lorem perspiciatis repudiandae ad nesciunt deleniti facilisi, est orci libero.",
-          },
-        ]
-      },
-    ];
+
+    // fetch all products from db 
+    this.service.fetchProductData().subscribe(data => {
+      if (data) {
+        let products = [];
+        for (let key in data) {
+          products.push(data[key]);
+          this.productIds.push(key);
+        }
+
+        //sort product according to date added
+        products.sort(function (a, b) {
+          let dateA = new Date(a.dateAdded).getTime(), dateB = new Date(b.dateAdded).getTime();
+          return dateA - dateB;
+        });
+
+        // only show 4 products here 
+        for (let i = 0; i < products.length; i++) {
+          if (i >= 4) {
+            break;
+          }
+          this.productData.push(products[i]);
+        }
+      }
+    }, error => console.log(error));
+
   }
 
 }
+
